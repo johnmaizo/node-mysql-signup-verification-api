299e45d4a8d715d2ddcbc06c45bcc0a731ab7e09
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-08-22 13:13:05 +0800

    Minor update: Adding/Correcting comments on relatioship.js

--- _helpers/relationship.js
+++ _helpers/relationship.js
@@ -1,54 +1,64 @@
 function defineRelationships(db) {
     // define relationships
 
-    // ! Account
+    // ! Account -> Refresh Token
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
 
-    // ! Department
+    // ! Department -> Course
     db.Department.hasMany(db.Course, { foreignKey: 'department_id' });
     db.Course.belongsTo(db.Department, { foreignKey: 'department_id' });
     
+    // ! Course -> Subject Info
     db.Course.hasMany(db.SubjectInfo, { foreignKey: 'course_id' });
     db.SubjectInfo.belongsTo(db.Course, { foreignKey: 'course_id' });
 
-    // ! Students
+    // ! Students -> Student Contact
     db.Student.hasMany(db.StudentContact, { foreignKey: 'student_id' });
     db.StudentContact.belongsTo(db.Student, { foreignKey: 'student_id' });
 
+    // ! Student -> Student Family
     db.Student.hasMany(db.StudentFamily, { foreignKey: 'student_id' });
     db.StudentFamily.belongsTo(db.Student, { foreignKey: 'student_id' });
 
+    // ! Student -> Student Current Academic
     db.Student.hasMany(db.StudentCurrentAcademic, { foreignKey: 'student_id' });
     db.StudentCurrentAcademic.belongsTo(db.Student, { foreignKey: 'student_id' });
 
+    // ! Student -> Academic History
     db.Student.hasMany(db.AcademicHistory, { foreignKey: 'student_id' });
     db.AcademicHistory.belongsTo(db.Student, { foreignKey: 'student_id' });
 
+    // ! Department Student Current Academic
     db.Department.hasMany(db.StudentCurrentAcademic, { foreignKey: 'department_id' });
     db.StudentCurrentAcademic.belongsTo(db.Department, { foreignKey: 'department_id' });
 
-    // ! Room
+    // ! Room -> Schedule
     db.RoomInfo.hasMany(db.Schedule, { foreignKey: 'room_id' });
     db.Schedule.belongsTo(db.RoomInfo, { foreignKey: 'room_id' });
 
-    // ! Student School Detail
+    // ! Student -> Students School Details
     db.Student.hasMany(db.StudentSchoolDetail, { foreignKey: 'student_id' });
     db.StudentSchoolDetail.belongsTo(db.Student, { foreignKey: 'student_id' });
+
+    // ! Course -> Student School Details
     db.Course.hasMany(db.StudentSchoolDetail, { foreignKey: 'course_id' });
     db.StudentSchoolDetail.belongsTo(db.Course, { foreignKey: 'course_id' });
+
+    // ! Department -> Student School Details
     db.Department.hasMany(db.StudentSchoolDetail, { foreignKey: 'department_id' });
     db.StudentSchoolDetail.belongsTo(db.Department, { foreignKey: 'department_id' });
 
-
-    // ! Student Subject
+    // ! Student -> Student Subject
     db.Student.hasMany(db.StudentSubject, { foreignKey: 'student_id' });
     db.StudentSubject.belongsTo(db.Student, { foreignKey: 'student_id' });
+
+    // ! Subject Info -> Student Subject
     db.SubjectInfo.hasMany(db.StudentSubject, { foreignKey: 'subject_code' });
     db.StudentSubject.belongsTo(db.SubjectInfo, { foreignKey: 'subject_code' });
 
 
-    // ! Teacher
+    // ! Department -> Teacher Info
     db.Department.hasMany(db.TeacherInfo, { foreignKey: 'department_id' });
     db.TeacherInfo.belongsTo(db.Department, { foreignKey: 'department_id' });
 
